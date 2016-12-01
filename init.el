bd535394f8d6d953c44499942c547f557ac19f7b
KeithZhang <g.fengling.11@gmail.com>
2016-12-01 14:31:31 +0800

    修改配置

--- init.el
+++ init.el
@@ -13,12 +13,15 @@
 ;; 禁止生成备份文件，如:init.el~
 (setq make-backup-files nil)
 
+;; 禁止auto-save
+(setq auto-save-default nil)
+
 ;; 显示行号
 (global-linum-mode t)
 
 ;; 显示当前行
 (global-hl-line-mode t)
- 
+
 ;; 定义打开文件或者目录的方法
 (defun open-my-init-file()
   (interactive)
@@ -56,6 +59,7 @@
 (require 'hungry-delete)
 (global-hungry-delete-mode t)
 
+;; swiper功能，搜索，查找文件等会在下方打开小窗口方便选择
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (global-set-key "\C-s" 'swiper)
@@ -63,5 +67,41 @@
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;; 自动补全括号功能
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
+;; 所有js文件使用js2-mode
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+;; 执行js的环境
+(require 'nodejs-repl)
+
+;; 当系统是mac os的时候，启用exec-path-from-shell-initialize
+;; 作用是本地装了node，在emacs中能调用到
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; 外部修改文件后，emacs重新加载
+(global-auto-revert-mode t)
+
+;; buffer会在底部出现而且光标会自动到底部窗口内，不需要经常切窗口
+(require 'popwin)
+(popwin-mode t)
+
+;; 自定义变量，一般是配置一些常用的变量但是又很难手动输入的
+(abbrev-mode t)
+(define-abbrev-table 'global-abbrev-table '(
+					    ;; Microsoft
+					    ("8mc" "Microsoft")
+					    ))
+
+
+
+
